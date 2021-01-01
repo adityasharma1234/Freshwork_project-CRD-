5a7cf16cd11e1899d036a9d8e4b8ef480f0e39bf
Aditya Sharma <32771292+adityasharma1234@users.noreply.github.com>
2021-01-01 21:02:34 +0530

    Update data.mjs

--- data.mjs
+++ data.mjs
@@ -4,41 +4,32 @@ export class db {
         this.data = {}
         this.dbPath = ''
     }
-
-    /**
-     * @param {string} path If path is given in Argument
-     * @param {undefined} path If path is not given in Argument
-     */
     async start(path) {
         return new Promise(async (resolved, rejected) => {
             //Checking if path is given
             if (path) {
-                console.log('\n---Instantiating DB instance in given Path---\n')
+                console.log('\n---\n')
             } else {
-                //If path is not given,Starting db in current working directory
                 console.log('\n---Instantiating DB instance in Default Path---\n')
                 path = process.cwd()
             }
-
-            //Checking if given path exists in the File System
             try {
                 if (!fs.existsSync(path)) {
                     throw ('!!!Filepath does not exists.Please give Absolute path!!!\n')
 
                 }
-                console.log('---Checking for previous DB---\n')
+                console.log('If_Available\n')
 
                 try {
-                    //Checking if Existing DB is present in Given Path
                     if (!fs.existsSync(path + '/db.json')) {
-                        //If no DB detected Creating new One.
+
                         await fs.writeFile(path + '/db.json', JSON.stringify(this.data), function (err) {
                             if (err) {
                                 console.log(err)
                                 rejected(err)
                             } else {
-                                console.log(`---DB doest not exists---\n---creating new one @ ${path+'db.json'}---\n`)
-                                console.log('-------Database Loaded-------\n')
+                                console.log(`---N/a---\n---creating new one @ ${path+'db.json'}---\n`)
+                                console.log('Load\n')
                                 resolved(true)
                             }
                         })
@@ -53,8 +44,8 @@ export class db {
                                 this.data = JSON.parse(fileData)
                                 this.dbPath = path + 'db.json'
                                 console.log(`-----Existing DB detected @ ${path+'db.json'}\n`)
-                                console.log('-----Loading existing DB-----\n')
-                                console.log('-------Database Loaded-------\n')
+                                console.log('Load\n')
+                                console.log(' Loaded\n')
                                 resolved(true);
                             }
                         })
@@ -67,40 +58,17 @@ export class db {
                 console.log(error)
                 rejected(false)
             }
-        });
-
-
-    }
-
-    /**
-     * @description Create new value in DB
-     * @param {string} key Key of Data
-     * @param {object} value value of Data
-     * @returns {Promise} Resolve on Successful Creation. Reject on any errror.
-     */
+        });}
     async create(key, value) {
         return new Promise(async (resolved, rejected) => {
             const rejectReason = []
-            console.log('------Inserting------')
+            console.log('--')
             console.log(`${key} - ${JSON.stringify(value)}`)
-            //Checking for DB size limit -- max(1GB)
             if (Buffer.byteLength(JSON.stringify(this.data)) / 1024 > 1024) {
                 rejectReason.push(`Datastore size limit exceding 1GB. Cannot create.`)
                 rejected(rejectReason)
                 return
             }
-            /* 
-                ----Reqired Validation----
-                Key:
-                    1.Not null
-                    2.Type of String
-                    3.Min of 1 char and Max of 32 char
-                    4.Key should be Unique
-                value:
-                    1.Not null or Empty
-                    2.Type of Object
-                    3.Max size of 16KB.
-            */
             if (key == null) {
                 rejectReason.push('Key should not be null')
             }
@@ -145,14 +113,9 @@ export class db {
 
         });
     }
-    /**
-     * @description Read value from DB
-     * @param {string} key Key of Data
-     * @returns {Promise} Resolve on Successful Read. Reject on any errror.
-     */
     async read(key) {
         return new Promise((resolved, rejected) => {
-            console.log('------Reading--------')
+            console.log('Read')
             console.log('Key-', key)
             //Checking key exists in DB
             if (!Object.keys(this.data).includes(key)) {
@@ -166,13 +129,9 @@ export class db {
             }
         });
     }
-    /**
-     * @description Create new value in DB
-     * @param {string} key Key of Data
-     * @returns {Promise} Resolve on Successful Delete. Reject on any errror.
-     */
+rns {Promise} Resolve on Successful Delete. Reject on any errror.
     async delete(key) {
-        console.log('------Deleting--------')
+        console.log('Delete')
         console.log('Key-', key)
         return new Promise(async (resolved, rejected) => {
             //Checking key exists in DB
@@ -200,8 +159,5 @@ export class db {
                     .catch((error) => {
                         rejected(error)
                     })
-                console.log('Deleted\n---------------------\n')
-            }
-        });
-    }
-}
+                console.log('Deletion_Done\n')
+            }});}}
